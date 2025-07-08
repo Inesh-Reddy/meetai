625a0483351fef704bd2a2cc56d9b46656f4d69a
Inesh-Reddy <ineshreddy007@gmail.com>
2025-07-08 13:03:24 +0530

    auth setup and tested basic authorization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,52 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/db/auth-client";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center h-screen">
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-      <Button variant={"outline"} >Meet.AI App</Button>
+  const onSignUp = () => {
+    authClient.signUp.email({
+      name,
+      email,
+      password,
+    });
+  };
+
+  return (
+    <div>
+      <Input
+        type="name"
+        placeholder="UserName"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      ></Input>
+      <br />
+      <Input
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      ></Input>
+      <br />
+      <Input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      ></Input>
+      <br />
+      <Button onClick={onSignUp}>SignUp</Button>
     </div>
   );
 }
