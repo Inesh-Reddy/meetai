9481ebc5f72bb736b54210dd673a7e43f7149b4d
Inesh-Reddy <ineshreddy007@gmail.com>
2025-07-11 13:37:21 +0530

    adding login and logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ export default function Home() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const { data: session } = authClient.useSession();
 
   const onSignUp = () => {
     authClient.signUp.email({
@@ -16,37 +20,81 @@ export default function Home() {
       password,
     });
   };
+  const onLogin = () => {
+    authClient.signIn.email({
+      email,
+      password,
+      callbackURL: "/",
+    });
+  };
+
+  const signOut = () => {
+    authClient.signOut();
+  };
+
+  if (session) {
+    return (
+      <div>
+        <h2>logged in as {session.user.name}</h2>
+        <Button onClick={signOut}>Signout</Button>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <Input
-        type="name"
-        placeholder="UserName"
-        value={name}
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-      ></Input>
-      <br />
-      <Input
-        type="email"
-        placeholder="email"
-        value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      ></Input>
-      <br />
-      <Input
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      ></Input>
-      <br />
-      <Button onClick={onSignUp}>SignUp</Button>
+      <div>
+        <Input
+          type="name"
+          placeholder="UserName"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></Input>
+        <br />
+        <Input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></Input>
+        <br />
+        <Input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></Input>
+        <br />
+        <Button onClick={onSignUp}>SignUp</Button>
+      </div>
+
+      <div>
+        <Input
+          type="email"
+          placeholder="loginemail"
+          value={loginEmail}
+          onChange={(e) => {
+            setLoginEmail(e.target.value);
+          }}
+        ></Input>
+        <br />
+        <Input
+          type="password"
+          placeholder="loginpassword"
+          value={loginPassword}
+          onChange={(e) => {
+            setLoginPassword(e.target.value);
+          }}
+        ></Input>
+        <br />
+        <Button onClick={onLogin}>Login</Button>
+      </div>
     </div>
   );
 }
