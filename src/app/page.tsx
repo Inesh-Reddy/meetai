a24d970f7d750f969f0e03b73dce79e842ca83e1
Inesh-Reddy <ineshreddy007@gmail.com>
2025-07-14 16:49:10 +0530

    SignUP Signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,18 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { authClient } from "@/db/auth-client";
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
+// import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useRef } from "react";
 
 export default function Home() {
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const passRef = useRef<HTMLInputElement>(null);
+
+  // Use useRouter to get the router object
+  const router = useRouter();
 
   const onSignup = () => {
     const name = nameRef.current?.value || "name";
@@ -23,7 +28,8 @@ export default function Home() {
       },
       {
         onSuccess: () => {
-          redirect("/Dashboard");
+          router.push("/Dashboard");
+          // redirect("/Dashboard");
         },
       }
     );
@@ -31,9 +37,9 @@ export default function Home() {
 
   return (
     <div>
-      <Input ref={nameRef} placeholder="name"></Input>
-      <Input ref={emailRef} placeholder="email"></Input>
-      <Input ref={passRef} placeholder="pass"></Input>
+      <Input ref={nameRef} placeholder="name" />
+      <Input ref={emailRef} placeholder="email" />
+      <Input ref={passRef} placeholder="pass" />
       <Button onClick={onSignup}>Signup</Button>
     </div>
   );
